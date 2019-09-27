f672e80e5df84d70cf6f4785ac80ca01a0e1c9ab
John Thielman <john.thielman@gmail.com>
2019-09-27 13:50:33 -0500

    enter button done, I hope

--- javascripts/components/order.js
+++ javascripts/components/order.js
@@ -2,10 +2,34 @@ import bread from './bread.js';
 import utilities from '../helpers/utilities.js';
 
 const shapeEndSandwich = (stuff) => {
-    let domString2 = '';
+    let domString2 = '<ul class="list-group list-group-flush">';
     for (let i = 0; i < stuff.length; i++) {
-        domString2 += `<h5>${stuff[i].name}</h5>`
+        domString2 += `
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            <div>
+                <h5>${stuff[i].name}</h5>
+            </div>
+            <div>
+                <h5>$${(stuff[i].price / 100).toFixed(2)}</h5>
+            </div>
+        </li>
+        `
     };
+    let cost = 0;
+    for (let j = 0; j < stuff.length; j++) {
+        cost += stuff[j].price;
+    }
+    domString2 += `
+        <li id="total-cost" class="list-group-item d-flex justify-content-between align-items-center">
+            <div>
+                <h4>Total:</h4>
+            </div>
+            <div>
+                <h4>$${(cost / 100).toFixed(2)}</h4>
+            </div>
+        </li>
+        `
+    domString2 += '</ul>'
     utilities.printToDom('order', domString2);
 };
 
